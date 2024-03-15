94e7f6af0f2f2e249b14df56a4972e8035e72743
Potato-66 <a3392608@163.com>
2024-03-15 11:28:34 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 2
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
